1473e0f33fc3cba4498d4907cd4a7c2f60de361d
g4s-la <labworkspace@yandex.ru>
2021-06-17 12:46:17 +0300

    Main screen + clickable fragment referring to movie details

---
+++ lesson3/src/main/java/com/example/lesson3/FragmentMoviesList.kt
@@ -0,0 +1,17 @@
+package com.example.lesson3
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.lesson2.R
+
+class FragmentMoviesList : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_movies_list, container, false)
+}
