4ea77d54c1d93f52559e9954e14231fc7d6a33c1
g4s-la <labworkspace@yandex.ru>
2021-06-28 09:51:20 +0300

    Init lesson4

--- lesson3/src/main/java/com/example/lesson4/FragmentMoviesList.kt
+++ lesson3/src/main/java/com/example/lesson4/FragmentMoviesList.kt
@@ -1,4 +1,4 @@
-package com.example.lesson3
+package com.example.lesson4
 
 import androidx.fragment.app.Fragment
 import com.example.lesson2.R
---
+++ lesson4/src/main/java/com/example/lesson4/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.lesson4
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.lesson2.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .add(R.id.main_container, FragmentMoviesList())
+                    .commit()
+        }
+
+        val cardFilm = findViewById<View>(R.id.main_container)
+        cardFilm.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                    .add(R.id.main, FragmentMoviesDetails())
+                    .addToBackStack(null)
+                    .commit()
+        }
+    }
+}
