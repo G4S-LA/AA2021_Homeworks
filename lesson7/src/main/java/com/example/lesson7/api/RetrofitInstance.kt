6b3ab6a448803b2a57fe033a9a9d422c6c414e5e
g4s-la <labworkspace@yandex.ru>
2021-07-20 12:18:09 +0300

    Add logger and imageUrlAppender

--- lesson7/src/main/java/com/example/lesson7/api/RetrofitInstance.kt
+++ lesson7/src/main/java/com/example/lesson7/api/RetrofitInstance.kt
@@ -1,5 +1,7 @@
 package com.example.lesson7.api
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,14 +10,27 @@ object RetrofitInstance {
     private const val BASE_URL = "https://api.themoviedb.org/3/"
     const val API_KEY = "06d6cb823d6b37952d2dab5da2f365bf"
 
+    private val logger by lazy {
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    private val okHttp = OkHttpClient.Builder().addInterceptor(logger)
+
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttp.build())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
 
     val api: MoviesApi by lazy {
         retrofit.create(MoviesApi::class.java)
+    }
+
+    val imageUrlAppender by lazy {
+        ImageUrlAppender()
     }
 }
